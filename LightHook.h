9858265e19963aab257f557a8c8b260357191eb1
Misty <gyc990326@gmail.com>
2023-05-08 23:59:00 +0800

    Initial version

---
+++ LightHook.h
@@ -0,0 +1,377 @@
+#ifndef LIGHT_HOOK
+#define LIGHT_HOOK
+
+/*
+ * LightHook
+ * webpage: https://tulach.cc
+ * repo: https://github.com/SamuelTulach/LightHook
+ */
+
+#define HOOK_R (*b >> 4)
+#define HOOK_C (*b & 0xF)
+
+static const unsigned char PREFIXES[] = { 0xF0, 0xF2, 0xF3, 0x2E, 0x36, 0x3E, 0x26, 0x64, 0x65, 0x66, 0x67 };
+static const unsigned char OP1_MODRM[] = { 0x62, 0x63, 0x69, 0x6B, 0xC0, 0xC1, 0xC4, 0xC5, 0xC6, 0xC7, 0xD0, 0xD1, 0xD2, 0xD3, 0xF6, 0xF7, 0xFE, 0xFF };
+static const unsigned char OP1_IMM8[] = { 0x6A, 0x6B, 0x80, 0x82, 0x83, 0xA8, 0xC0, 0xC1, 0xC6, 0xCD, 0xD4, 0xD5, 0xEB };
+static const unsigned char OP1_IMM32[] = { 0x68, 0x69, 0x81, 0xA9, 0xC7, 0xE8, 0xE9 };
+static const unsigned char OP2_MODRM[] = { 0x0D, 0xA3, 0xA4, 0xA5, 0xAB, 0xAC, 0xAD, 0xAE, 0xAF };
+
+/**
+ * \brief Checks if given byte is present in buffer
+ * \param buffer Input buffer to search in
+ * \param maxLength Buffer size in bytes
+ * \param value Single byte value to search for
+ * \return Non-null if found, null if not
+ */
+static int FindByte(const unsigned char* buffer, const unsigned long long maxLength, const unsigned char value)
+{
+	for (unsigned long long i = 0; i < maxLength; i++)
+	{
+		if (buffer[i] == value)
+			return 1;
+	}
+
+	return 0;
+}
+
+/**
+ * \brief Check for ModR/M byte and adjust the buffer pointer accordingly
+ * \param buffer Pointer to the current buffer address
+ * \param addressPrefix Instruction has legacy address size overwrite prefix
+ */
+static void ParseModRM(unsigned char** buffer, const int addressPrefix)
+{
+	const unsigned char modRm = *++ * buffer;
+
+	if (!addressPrefix || (addressPrefix && **buffer >= 0x40))
+	{
+		int hasSib = 0;
+		if (**buffer < 0xC0 && (**buffer & 0b111) == 0b100 && !addressPrefix)
+			hasSib = 1, (*buffer)++;
+
+		if (modRm >= 0x40 && modRm <= 0x7F)
+			(*buffer)++;
+		else if ((modRm <= 0x3F && (modRm & 0b111) == 0b101) || (modRm >= 0x80 && modRm <= 0xBF))
+			*buffer += (addressPrefix) ? 2 : 4;
+		else if (hasSib && (**buffer & 0b111) == 0b101)
+			*buffer += (modRm & 0b01000000) ? 1 : 4;
+	}
+	else if (addressPrefix && modRm == 0x26)
+		*buffer += 2;
+}
+
+/**
+ * \brief Get size of basic instructions for x86_64 (AMD64) platform
+ * \param address Address of instruction to get length of
+ * \return Size in bytes of instruction
+ */
+static int GetInstructionSize(const void* address)
+{
+	/*
+	 * Based on length-disassembler by @Nomade040
+	 * https://github.com/Nomade040/length-disassembler
+	 */
+
+	unsigned long long offset = 0;
+	int operandPrefix = 0, addressPrefix = 0, rexW = 0;
+	unsigned char* b = (unsigned char*)address;
+
+	for (int i = 0; i < 14 && FindByte(PREFIXES, sizeof(PREFIXES), *b) || HOOK_R == 4; i++, b++)
+	{
+		if (*b == 0x66)
+			operandPrefix = 1;
+		else if (*b == 0x67)
+			addressPrefix = 1;
+		else if (HOOK_R == 4 && HOOK_C >= 8)
+			rexW = 1;
+	}
+
+	if (*b == 0x0F)
+	{
+		b++;
+		if (*b == 0x38 || *b == 0x3A)
+		{
+			if (*b++ == 0x3A)
+				offset++;
+
+			ParseModRM(&b, addressPrefix);
+		}
+		else
+		{
+			if (HOOK_R == 8)
+				offset += 4;
+			else if ((HOOK_R == 7 && HOOK_C < 4) || *b == 0xA4 || *b == 0xC2 || (*b > 0xC3 && *b <= 0xC6) || *b == 0xBA || *b == 0xAC)
+				offset++;
+
+			if (FindByte(OP2_MODRM, sizeof(OP2_MODRM), *b) || (HOOK_R != 3 && HOOK_R > 0 && HOOK_R < 7) || *b >= 0xD0 || (HOOK_R == 7 && HOOK_C != 7) || HOOK_R == 9 || HOOK_R == 0xB || (HOOK_R == 0xC && HOOK_C < 8) || (HOOK_R == 0 && HOOK_C < 4))
+				ParseModRM(&b, addressPrefix);
+		}
+	}
+	else
+	{
+		if ((HOOK_R == 0xE && HOOK_C < 8) || (HOOK_R == 0xB && HOOK_C < 8) || HOOK_R == 7 || (HOOK_R < 4 && (HOOK_C == 4 || HOOK_C == 0xC)) || (*b == 0xF6 && !(*(b + 1) & 48)) || FindByte(OP1_IMM8, sizeof(OP1_IMM8), *b))
+			offset++;
+		else if (*b == 0xC2 || *b == 0xCA)
+			offset += 2;
+		else if (*b == 0xC8)
+			offset += 3;
+		else if ((HOOK_R < 4 && (HOOK_C == 5 || HOOK_C == 0xD)) || (HOOK_R == 0xB && HOOK_C >= 8) || (*b == 0xF7 && !(*(b + 1) & 48)) || FindByte(OP1_IMM32, sizeof(OP1_IMM32), *b))
+			offset += (rexW) ? 8 : (operandPrefix ? 2 : 4);
+		else if (HOOK_R == 0xA && HOOK_C < 4)
+			offset += (rexW) ? 8 : (addressPrefix ? 2 : 4);
+		else if (*b == 0xEA || *b == 0x9A)
+			offset += operandPrefix ? 4 : 6;
+
+		if (FindByte(OP1_MODRM, sizeof(OP1_MODRM), *b) || (HOOK_R < 4 && (HOOK_C < 4 || (HOOK_C >= 8 && HOOK_C < 0xC))) || HOOK_R == 8 || (HOOK_R == 0xD && HOOK_C >= 8))
+			ParseModRM(&b, addressPrefix);
+	}
+
+	return (int)(++b + offset - (unsigned char*)address);
+}
+
+/**
+ * \brief Substitute for memcpy function
+ * \param destination Target address to copy date into
+ * \param source Source address to copy data from
+ * \param size Amount of bytes to copy
+ */
+static void CopyMemory(void* destination, void* source, unsigned long long size)
+{
+	unsigned char* dst = (unsigned char*)destination;
+	unsigned char* src = (unsigned char*)source;
+	for (unsigned long long i = 0; i < size; i++)
+		dst[i] = src[i];
+}
+
+typedef struct _HookInformation
+{
+	int Enabled;
+	int BytesToCopy;
+	unsigned char OriginalBuffer[32];
+	void* OriginalFunction;
+	void* TargetFunction;
+	void* Trampoline;
+} HookInformation;
+
+static const unsigned char JUMP_CODE[] = { 0x49, 0xBF, 0xED, 0xFE, 0xED, 0xFE, 0xED, 0xFE, 0xAD, 0xDE, 0x41, 0xFF, 0xE7 };
+
+/**
+ * \brief Prepare hook information structure and backup original function bytes that will be used for the trampoline
+ * \param originalFunction Function that will be hooked
+ * \param targetFunction Function that will be called
+ * \return Hook information structure
+ */
+static HookInformation CreateHook(void* originalFunction, void* targetFunction)
+{
+	HookInformation information;
+	information.Enabled = 0;
+	information.Trampoline = 0;
+	information.OriginalFunction = originalFunction;
+	information.TargetFunction = targetFunction;
+
+	int size = 0;
+	while (size < sizeof(JUMP_CODE))
+		size += GetInstructionSize((unsigned char*)originalFunction + size);
+
+	information.BytesToCopy = size;
+	CopyMemory(information.OriginalBuffer, originalFunction, size);
+
+	return information;
+}
+
+#ifdef _WIN64
+#ifdef _KERNEL_MODE
+#ifndef _EFI
+#include <ntifs.h>
+#include <intrin.h>
+#endif
+#else
+#define WIN32_NO_STATUS
+#define WIN32_LEAN_AND_MEAN
+#include <Windows.h>
+#undef CopyMemory
+#endif
+#endif
+#ifdef __linux__
+#include <stdio.h>
+#include <sys/mman.h>
+#include <assert.h>
+#include <errno.h>
+#endif
+#ifdef _EFI
+#include <efi.h>
+#include <efilib.h>
+#endif
+
+/**
+ * \brief Allocate RWX memory
+ * \param size Size in bytes
+ * \return Pointer to allocated memory region
+ */
+static void* PlatformAllocate(const unsigned long long size)
+{
+#ifdef _EFI
+	const unsigned long long numberOfPages = 1 + size / 1024;
+	EFI_PHYSICAL_ADDRESS physicalAddress;
+	gBS->AllocatePages(AllocateAnyPages, EfiRuntimeServicesCode, numberOfPages, &physicalAddress);
+	return (void*)physicalAddress;
+#endif
+#ifdef _WIN64
+#ifdef _KERNEL_MODE
+#ifndef _EFI
+	return ExAllocatePool(NonPagedPoolExecute, size);
+#endif
+#else
+	return VirtualAlloc(0, size, MEM_RESERVE | MEM_COMMIT, PAGE_EXECUTE_READWRITE);
+#endif
+#else
+#ifdef __linux__
+	return mmap(0, size, PROT_READ | PROT_WRITE | PROT_EXEC, MAP_ANONYMOUS | MAP_PRIVATE, 0, 0);
+#endif
+	(void)size;
+	return 0;
+#endif
+}
+
+/**
+ * \brief Free/release memory previously allocated with PlatformAllocate
+ * \param address Pointer to memory region
+ * \param size Size in bytes
+ */
+static void PlatformFree(void* address, const unsigned long long size)
+{
+#ifdef _EFI
+	const unsigned long long numberOfPages = 1 + size / 1024;
+	gBS->FreePages((EFI_PHYSICAL_ADDRESS)address, numberOfPages);
+	return;
+#endif
+#ifdef _WIN64
+#ifdef _KERNEL_MODE
+#ifndef _EFI
+	(void)size;
+	ExFreePool(address);
+#endif
+#else
+	VirtualFree(address, size, MEM_RELEASE);
+#endif
+#ifdef __linux__
+	munmap(address, size);
+#endif
+#endif
+}
+
+#define PROTECTION_READ_WRITE_EXECUTE 0xfffffffffffe
+
+/**
+ * \brief Change memory protection flags in usermode, disable write protection bit in cr0 in kernel mode
+ * \param address Memory location
+ * \param size Size in bytes (will likely be rounded to one page)
+ * \param protection Use PROTECTION_READ_WRITE_EXECUTE to make region RWX, otherwise platform specific
+ * \return Original protection value
+ */
+static unsigned long long PlatformProtect(void* address, unsigned long long size, unsigned long long protection)
+{
+#ifdef _WIN64
+#ifdef _KERNEL_MODE
+	(void)size;
+	(void)address;
+	if (protection == PROTECTION_READ_WRITE_EXECUTE)
+	{
+		_disable();
+
+		unsigned long long cr0 = __readcr0();
+		unsigned long long originalCr0 = cr0;
+		cr0 &= ~(1UL << 16);
+		__writecr0(cr0);
+
+		return originalCr0;
+	}
+	else
+	{
+		__writecr0(protection);
+		_enable();
+		return 0;
+	}
+#else
+	if (protection == PROTECTION_READ_WRITE_EXECUTE)
+		protection = PAGE_EXECUTE_READWRITE;
+
+	unsigned long original;
+	VirtualProtect(address, size, (unsigned long)protection, &original);
+	return original;
+#endif
+#endif
+#ifdef __linux__
+	(void)size;
+	if (protection == PROTECTION_READ_WRITE_EXECUTE)
+		protection = PROT_READ | PROT_WRITE | PROT_EXEC;
+	else
+		protection = PROT_READ | PROT_EXEC; // unfortunately no way to read the original without parsing /proc/self/maps
+
+	int pageSize = getpagesize();
+	unsigned long long pageOffset = (unsigned long long)address % pageSize;
+	address -= pageOffset;
+
+	int status = mprotect(address, pageSize, protection);
+	assert(status == 0);
+
+	return protection;
+#endif
+}
+
+#define CREATE_JUMP(name, targetAddress) \
+	unsigned char name[sizeof(JUMP_CODE)]; \
+	CopyMemory(name, (unsigned char*)JUMP_CODE, sizeof(JUMP_CODE)); \
+	*(unsigned long long*)((unsigned long long)name + 2) = (unsigned long long)targetAddress
+
+/**
+ * \brief Actually perform the trampoline hook
+ * \param information Information structure created with CreateHook
+ * \return Non-zero when successful, zero when fail
+ */
+static int EnableHook(HookInformation* information)
+{
+	if (information->Enabled)
+		return 1;
+
+	const int bufferSize = sizeof(JUMP_CODE) + information->BytesToCopy;
+	unsigned char* buffer = (unsigned char*)PlatformAllocate(bufferSize);
+	if (!buffer)
+		return 0;
+
+	information->Trampoline = buffer;
+	CopyMemory(buffer, information->OriginalBuffer, information->BytesToCopy);
+
+	CREATE_JUMP(originalJump, information->OriginalFunction + information->BytesToCopy);
+	CopyMemory(buffer + information->BytesToCopy, originalJump, sizeof(JUMP_CODE));
+
+	CREATE_JUMP(targetJump, information->TargetFunction);
+	unsigned long long originalProtection = PlatformProtect(information->OriginalFunction, information->BytesToCopy, PROTECTION_READ_WRITE_EXECUTE);
+	CopyMemory(information->OriginalFunction, targetJump, sizeof(JUMP_CODE));
+	PlatformProtect(information->OriginalFunction, information->BytesToCopy, originalProtection);
+
+	information->Enabled = 1;
+	return 1;
+}
+
+/**
+ * \brief Remove the trampoline hook
+ * \param information Information structure created with CreateHook
+ * \return Non-zero when successful, zero when fail
+ */
+static int DisableHook(HookInformation* information)
+{
+	if (!information->Enabled)
+		return 1;
+
+	unsigned long long originalProtection = PlatformProtect(information->OriginalFunction, information->BytesToCopy, PROTECTION_READ_WRITE_EXECUTE);
+	CopyMemory(information->OriginalFunction, information->OriginalBuffer, information->BytesToCopy);
+	PlatformProtect(information->OriginalFunction, information->BytesToCopy, originalProtection);
+
+	PlatformFree(information->Trampoline, sizeof(JUMP_CODE) + information->BytesToCopy);
+
+	information->Enabled = 0;
+	return 1;
+}
+
+#endif
---
+++ test.c
@@ -0,0 +1,230 @@
+#define _GNU_SOURCE
+#include <dlfcn.h>
+#include <fcntl.h>
+#include <libudev.h>
+#include <netinet/in.h>
+#include <stdint.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <sys/uio.h>
+#include <unistd.h>
+#include "LightHook.h"
+
+typedef void *RSA;
+#define DISK_LABEL "UNRAID"
+#define BTRS_FORMAT \
+  "regGUID=%s&regTy=%s&regTo=\"%s\"&regTm=%s&regGen=0&regDays=0"
+
+typedef int (*RSA_PUBLIC_DECRYPT_FUNC)(int flen, unsigned char* from,
+                                       unsigned char* to, RSA* rsa,
+                                       int padding);
+
+static char* unraid_uuid = NULL;
+static char* unraid_name = NULL;
+static char* unraid_date = NULL;
+static char* unraid_version = NULL;
+RSA_PUBLIC_DECRYPT_FUNC rsa_public_decrypt;
+static HookInformation HOOKINFO_rsa_public_decrypt;
+
+int get_dev_path(char* buffer, size_t size);
+int get_usb_device(char* buffer, size_t size);
+int get_serial_string(char* buffer, size_t size);
+void read_file(char* buff_ptr, char* base_ptr, char* file_ptr, char* file);
+static int hook_RSA_public_decrypt(int flen, unsigned char* from, unsigned char* to,
+                       RSA* rsa, int padding);
+
+static void init_unraid_uuid() {
+  if (!unraid_uuid) {
+    unraid_uuid = (char*)malloc(1024);
+    strcpy(unraid_uuid, "1234-1234-1234-1234567890AB");
+    int err = get_serial_string(unraid_uuid, 1024);
+    if (err && getenv("UNRAID_UUID")) {
+      strcpy(unraid_uuid, getenv("UNRAID_UUID"));
+    }
+    unraid_name = getenv("UNRAID_NAME");
+    unraid_date = getenv("UNRAID_DATE");
+    unraid_version = getenv("UNRAID_VERSION");
+  }
+}
+
+static void init_unraid_hook() {
+  void *libcrypto = dlopen("/lib64/libcrypto.so.1.1", RTLD_NOW);
+  void *ori_rsa_public_decrypt = dlsym(libcrypto, "RSA_public_decrypt");
+  assert(ori_rsa_public_decrypt != NULL);
+
+  HOOKINFO_rsa_public_decrypt = CreateHook((void*)ori_rsa_public_decrypt, (void*)&hook_RSA_public_decrypt);
+  int status = EnableHook(&HOOKINFO_rsa_public_decrypt);
+  assert(status == 1);
+  rsa_public_decrypt = (RSA_PUBLIC_DECRYPT_FUNC)HOOKINFO_rsa_public_decrypt.Trampoline;
+}
+
+__attribute__((constructor)) void unraid_init() {
+  init_unraid_uuid();
+  init_unraid_hook();
+}
+
+const char* get_self_exe_name(int full) {
+  static char buffer[4096] = "";
+  readlink("/proc/self/exe", buffer, 4096);
+  if (full) {
+    return buffer;
+  }
+  char* ptr = &buffer[strlen(buffer)];
+  while (*ptr != '/') --ptr;
+  return (ptr + 1);
+}
+
+static int hook_RSA_public_decrypt(int flen, unsigned char* from, unsigned char* to,
+                       RSA* rsa, int padding) {
+  if (!strcmp(get_self_exe_name(0), "emhttpd") ||
+      !strcmp(get_self_exe_name(0), "shfs")) {
+    sprintf(to, BTRS_FORMAT, unraid_uuid, unraid_version, unraid_name,
+            unraid_date);
+    int len = strlen(to);
+    return len;
+  } else {
+    return rsa_public_decrypt(flen, from, to, rsa, padding);
+  }
+}
+
+/**** udev stuff ****/
+int get_dev_path(char* buffer, size_t size) {
+  char link_device[1024];
+  char real_device[1024];
+
+  sprintf(link_device, "/dev/disk/by-label/%s", DISK_LABEL);
+  char* rv = realpath(link_device, real_device);
+  if (!rv) return 2;
+
+  struct udev* udev;
+  struct udev_device* dev;
+  struct udev_enumerate* enumerate;
+  struct udev_list_entry *devices, *dev_list_entry;
+
+  int find = -1;
+
+  udev = udev_new();
+  if (!udev) {
+    return 1;
+  }
+
+  enumerate = udev_enumerate_new(udev);
+  if (!enumerate) {
+    return 1;
+  }
+
+  udev_enumerate_add_match_subsystem(enumerate, "block");
+  udev_enumerate_scan_devices(enumerate);
+
+  devices = udev_enumerate_get_list_entry(enumerate);
+  if (!devices) {
+    return 1;
+  }
+
+  udev_list_entry_foreach(dev_list_entry, devices) {
+    const char *path, *tmp;
+    unsigned long long disk_size = 0;
+
+    path = udev_list_entry_get_name(dev_list_entry);
+    dev = udev_device_new_from_syspath(udev, path);
+
+    if (strncmp(udev_device_get_devtype(dev), "partition", 9) != 0 &&
+        strncmp(udev_device_get_sysname(dev), "loop", 4) != 0) {
+      const char* devnode = udev_device_get_devnode(dev);
+      char* ptr = strstr(real_device, devnode);
+      if (ptr && ptr == real_device) {  // prefix
+        find = 0;
+        strcpy(buffer, udev_device_get_devpath(dev));
+      }
+    }
+
+    udev_device_unref(dev);
+  }
+
+  udev_enumerate_unref(enumerate);
+  udev_unref(udev);
+
+  return find;
+}
+
+int get_usb_device(char* buffer, size_t size) {
+  char dev_path[1024];
+  if (get_dev_path(dev_path, 1024)) {
+    return 2;
+  }
+
+  int slash_index[1024];
+  int slash_count = 0;
+  for (int i = 0; i < strlen(dev_path); ++i) {
+    if (dev_path[i] == '/') {
+      slash_index[slash_count++] = i;
+    }
+  }
+
+  int find = -1;
+  for (int i = slash_count - 1; i >= 0; --i) {
+    char usb_device[1024] = "/sys";
+    strcpy(&dev_path[slash_index[i]], "/serial");
+    strcat(usb_device, dev_path);
+    if (!access(usb_device, F_OK | R_OK)) {
+      strcpy(buffer, usb_device);
+      buffer[strlen(buffer) - strlen("/serial")] = '\0';
+      find = 0;
+      break;
+    }
+  }
+
+  return find;
+}
+
+void read_file(char* buff_ptr, char* base_ptr, char* file_ptr, char* file) {
+  strcpy(file_ptr, file);
+  FILE* fp = fopen(base_ptr, "r");
+  fscanf(fp, "%s", buff_ptr);
+  fclose(fp);
+}
+
+int get_serial_string(char* buffer, size_t size) {
+  char usb_device[1024];
+  if (get_usb_device(usb_device, 1024)) {
+    return 2;
+  }
+
+  char* file_ptr = &usb_device[strlen(usb_device)];
+  char* buff_ptr = buffer;
+
+  read_file(buff_ptr, usb_device, file_ptr, "/idVendor");
+  strcat(buff_ptr, "-");
+  buff_ptr += strlen(buff_ptr);
+
+  read_file(buff_ptr, usb_device, file_ptr, "/idProduct");
+  strcat(buff_ptr, "-");
+  buff_ptr += strlen(buff_ptr);
+
+  char raw_serial_buffer[1024];
+  read_file(raw_serial_buffer, usb_device, file_ptr, "/serial");
+  int serial_length = strlen(raw_serial_buffer);
+  char* serial_buffer = &raw_serial_buffer[serial_length - 16];
+  strcpy(buff_ptr, serial_buffer);
+  *(buff_ptr + 4) = '-';
+  strcpy(buff_ptr + 5, &serial_buffer[4]);
+
+  int offset = 'A' - 'a';
+  for (int i = 0; i < strlen(buffer); ++i) {
+    if (buffer[i] >= 'a' && buffer[i] <= 'z') {
+      buffer[i] += offset;
+    }
+  }
+
+  return 0;
+}
+
+int main() {
+  char serial[1024];
+  int err = get_serial_string(serial, 1024);
+  if (!err) {
+    printf("SERIAL: %s\n", serial);
+  }
+  return 0;
+}
